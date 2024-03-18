fdb7cd0baf3a9a09e63b0b0d7d5cc791b1bb6222
Lucille Amanda Jureidini <145380670+cdblast@users.noreply.github.com>
2024-03-18 11:33:25 -0700

    base game finished
    -Camera perspective changed
    -Spawn manager changed (Automated!)
    -Rigidbody and Box Collider added to animals
    Scripts added
    -DetectCollision
    
    -Animals can spawn out of player's bounds? fix

--- BasicGameplay/Assets/Scripts/PlayerController.cs
+++ BasicGameplay/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float verticalInput;
     public float speed = 10.0f;
     public float xRange = 10;
 
---
+++ BasicGameplay/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+
+    public GameObject[] animalPrefabs;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
+
+    private float startDelay = 2;
+    private float spawnInterval = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", startDelay,spawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void SpawnRandomAnimal()
+    {
+        //random position
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        //random animal
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Instantiate(animalPrefabs[animalIndex], spawnPos,
+        animalPrefabs[animalIndex].transform.rotation);
+    }
+}
