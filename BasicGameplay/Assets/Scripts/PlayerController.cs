24c645277858ca42f201caf901e92166f4066433
Lucille Amanda Jureidini <145380670+cdblast@users.noreply.github.com>
2024-03-19 09:09:08 -0700

    huzzah!
    added more movement
    pleaaseee fix aggressive animals

--- BasicGameplay/Assets/Scripts/PlayerController.cs
+++ BasicGameplay/Assets/Scripts/PlayerController.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
-    public float verticalInput;
     public float speed = 10.0f;
     public float xRange = 10;
 
     public GameObject projectilePrefab;
+
+    public float zMin;
+    public float zMax;
+    public float verticalInput;
+
+    public Transform projectileSpawnPoint;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,12 +25,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //feed em all
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
-        }
-
         //keep player in bounds
         if (transform.position.x < -xRange)
         {
@@ -34,7 +34,25 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+        if (transform.position.z < zMin)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zMin);
+        }
+        if (transform.position.z > zMax)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zMax);
+        }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+
+        //feed em all
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, projectileSpawnPoint.position, projectilePrefab.transform.rotation);
+        }
+
     }
 }
